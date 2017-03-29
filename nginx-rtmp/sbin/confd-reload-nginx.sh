5daeaab4d4dbac87e2cc2134e2a6f426c6a8cd6f
Ging.wu <wu.tongqing@gmail.com>
2017-03-29 13:17:03 +0800

    simple nginx rtmp

--- nginx-rtmp/sbin/confd-reload-nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -s /var/run/nginx.pid ]; then
-    echo "confd-reload-nginx.sh: reloading nginx..."
-    /usr/sbin/nginx -s reload
-else
-    echo "confd-reload-nginx.sh: ignoring reload because nginx is not yet running"
-fi
